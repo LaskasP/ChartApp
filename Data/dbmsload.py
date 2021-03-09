e4e37e9a0d54a358114d5c12af5aa25fd81e5df3
maximosstratis <maximosstratis@hotmail.com>
2021-03-09 23:00:16 +0200

    Update dbmsload.py

--- Data/dbmsload.py
+++ Data/dbmsload.py
@@ -17,26 +17,26 @@ cursor.execute("USE chartapp")
 tablenames = []
 
 # directory = os.path.join('H:\\', 'Projects\GitHub\ChartApp\Data\DBMSReady')
-directory = os.path.join('C:\\', 'Users\max\Documents\GitHub\ChartApp\Data\DBMSReady')
-for root, dirs, files in os.walk(directory):
+files_dir = "C:/Users/max/Documents/GitHub/ChartApp/Data/DBMSReady/"
+for root, dirs, files in os.walk(files_dir):
     for file in files:
         if file.endswith(".csv"):
             tablenames.append(file.split('.')[0])
 
 tables = []
 
-files_dir = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/"
+
 
 cursor.execute("CREATE TABLE Countries (country_name VARCHAR(50) NOT NULL, country_id int PRIMARY KEY)")
-cursor.execute("LOAD DATA INFILE '" + files_dir + "Countries.csv' INTO TABLE countries FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'")
+cursor.execute("LOAD DATA LOCAL INFILE '" + files_dir + "Countries.csv' INTO TABLE countries FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'")
 
 cursor.execute("CREATE TABLE Years (year_value INT NOT NULL, year_id int PRIMARY KEY)")
-cursor.execute("LOAD DATA INFILE '" + files_dir + "Years.csv' INTO TABLE years FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'")
+cursor.execute("LOAD DATA LOCAL INFILE '" + files_dir + "Years.csv' INTO TABLE years FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'")
 
 
 for name in tablenames:
     if name != "Countries" and name != "Years":
-        cursor.execute("CREATE TABLE " + name + " (country_id INT, year_id INT, value VARCHAR(20), FOREIGN KEY (country_id) REFERENCES Countries(country_id), FOREIGN KEY (year_id) REFERENCES Years(year_id), PRIMARY KEY (country_id,year_id))")
-        cursor.execute("LOAD DATA INFILE '" + files_dir + name + ".csv' INTO TABLE " + name + " FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'")
+        cursor.execute("CREATE TABLE " + name + " (country_id INT, year_id INT, value FLOAT, FOREIGN KEY (country_id) REFERENCES Countries(country_id), FOREIGN KEY (year_id) REFERENCES Years(year_id), PRIMARY KEY (country_id,year_id))")
+        cursor.execute("LOAD DATA LOCAL INFILE '" + files_dir + name + ".csv' INTO TABLE " + name + " FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'")
 
 cursor.close()
